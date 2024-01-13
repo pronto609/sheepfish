ec983ee8d7acc006ebead402c9cd67c4aa6992c7
Danaiel Brazhnyk <d.bragnik@sam-solutions.biz>
2024-01-13 15:36:21 +0200

    add relations

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
@@ -11,4 +12,8 @@ class Company extends Model
     use HasFactory;
     use SoftDeletes;
 
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
@@ -11,4 +12,8 @@ class Employee extends Model
     use HasFactory;
     use SoftDeletes;
 
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
